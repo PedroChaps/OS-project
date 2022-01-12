dbd92de098d8070337a87faceb4bc109747e74fe
pleaky2410 <andre.espirito.santo@tecnico.ulisboa.pt>
2022-01-12 11:12:53 +0000

    new data block read, the old one is stored in TR_read_with_diff_fh.c

--- tecnicofs-v2/fs/operations.c
+++ tecnicofs-v2/fs/operations.c
@@ -401,6 +401,24 @@ ssize_t tfs_write(int fhandle, void const *buffer, size_t to_write) {
     return (ssize_t) to_write;
 }
 
+ssize_t block_read(char * buffer, int const *block_of_blocks,int *n_blocks, size_t * block_offset, size_t * buffer_offset, inode_t * inode,size_t * to_read,size_t to_read_cpy, int type){
+
+        void * block = get_next_block(block_of_blocks,inode,*n_blocks,type);
+        if(block == NULL)
+            return -1;
+        //void * block1 = data_block_get(*(block_of_blocks + *n_blocks-1));
+
+        size_t what_to_read = min(*to_read, BLOCK_SIZE - *block_offset);
+        memcpy(buffer + *buffer_offset,block + *block_offset,what_to_read);
+
+        if(what_to_read == *to_read)
+            return (ssize_t) to_read_cpy;
+
+        *buffer_offset += what_to_read;
+        (*n_blocks)++;
+        *to_read = sub_or_zero(*to_read, what_to_read);
+        return 0;
+}
 
 ssize_t data_block_read(void *buffer, inode_t *inode, size_t offset, size_t to_read){
 
@@ -415,41 +433,21 @@ ssize_t data_block_read(void *buffer, inode_t *inode, size_t offset, size_t to_r
 
         /* If block offset isn't zero, we want to read just a part of the starting block */
         if (block_offset != 0) {
-            void *block = data_block_get(inode->i_data_block[n_blocks - 1]);
-
-            if (block == NULL)
+            ssize_t res = block_read(buffer,NULL,&n_blocks,&block_offset,&buffer_offset,inode,&to_read, to_read_cpy, DIRECT);
+            if (res == -1)
                 return -1;
-
-            /* The data is read and the block_offset is reseted */
-            size_t what_to_read = min(to_read, BLOCK_SIZE - block_offset);
-            memcpy(buffer + buffer_offset, block + block_offset, what_to_read);
-            buffer_offset += what_to_read;
-
-            /* If they match, it means everything was read, so returns */
-            if(what_to_read == to_read)
-                return (ssize_t) to_read_cpy;
-
-            /* Updates the variables accordingly*/
-            n_blocks++;
-            to_read = sub_or_zero(to_read, what_to_read);
+            else if (res == (ssize_t)to_read_cpy)
+                return res;
             block_offset = 0;
         }
 
         /* Keeps reading until the end of the direct blocks */
         while (n_blocks <= 10 && to_read > 0){
-            void * block = data_block_get(inode-> i_data_block[n_blocks-1]);
-            if (block == NULL)
+            ssize_t res = block_read(buffer,NULL,&n_blocks,&block_offset,&buffer_offset,inode,&to_read, to_read_cpy, DIRECT);
+            if (res == -1)
                 return -1;
-
-            size_t what_to_read = min(to_read, BLOCK_SIZE);
-            memcpy(buffer + buffer_offset, block, what_to_read);
-
-            if(what_to_read == to_read)
-                return (ssize_t) to_read_cpy;
-
-            buffer_offset += what_to_read;
-            n_blocks++;
-            to_read = sub_or_zero(to_read, what_to_read);
+            else if(res == (ssize_t) to_read_cpy)
+                return res;    
         }
 
     }
@@ -463,39 +461,25 @@ ssize_t data_block_read(void *buffer, inode_t *inode, size_t offset, size_t to_r
     if (block_of_blocks == NULL)
         return -1;
 
-    void *block = data_block_get(*(block_of_blocks + n_blocks-1));
-
-    if (block == NULL)
-        return -1;
-
     /* If block_offset isn't zero, we want to skip a part of the starting block (doesn't happen if
      * coming from direct blocks, because the block_offset is set to 0) */
     if (block_offset != 0) {
 
-        size_t what_to_read = min(to_read, BLOCK_SIZE - block_offset);
-        memcpy(buffer + buffer_offset, block + block_offset, what_to_read); //FIXME VER SE O QUE E PARA LER E MENOR QUE ESTE VALOR
-
-        if(what_to_read == to_read)
-            return (ssize_t) to_read_cpy;
-
-        buffer_offset += what_to_read;
-        n_blocks++;
-        to_read = sub_or_zero(to_read, what_to_read);
+        ssize_t res = block_read(buffer,block_of_blocks,&n_blocks,&block_offset,&buffer_offset,inode,&to_read, to_read_cpy, INDIRECT);
+        if (res == -1)
+            return -1;
+        else if(res == (ssize_t) to_read_cpy)
+            return res;
+        block_offset = 0;
     }
 
     /* Reads to the buffer while there are bytes to be read */
     while (to_read > 0){
-        void * block1 = data_block_get(*(block_of_blocks + n_blocks-1));
-
-        size_t what_to_read = min(to_read, BLOCK_SIZE);
-        memcpy(buffer + buffer_offset,block1,what_to_read);
-
-        if(what_to_read == to_read)
-            return (ssize_t) to_read_cpy;
-
-        buffer_offset += what_to_read;
-        n_blocks++;
-        to_read = sub_or_zero(to_read, what_to_read);
+        ssize_t res = block_read(buffer,block_of_blocks,&n_blocks,&block_offset,&buffer_offset,inode,&to_read, to_read_cpy, INDIRECT);
+        if (res == -1)
+            return -1;
+        else if(res == (ssize_t) to_read_cpy)
+            return res;
     }
 
     return -1;
